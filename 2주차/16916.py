fe5112f43757e0bb24842b0d0baaf4059ea88d23
BaeChangho <billyly96@gmail.com>
2022-04-29 19:17:02 +0900

    2주차

---
+++ 2주차/16916.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#16916
+str = input()
+S = list(str)
+find = input()
+P = list(find)
+answer = 0
+for i in range(len(S)):
+    if len(S)-i< len(P):
+        break
+    if S[i] == P[0]:
+        for j in range(len(P)-1):
+            if S[i+1+j] == P[j+1]:
+                answer = 1
+            elif S[i+1+j] != P[j+1]:
+                answer = 0
+                break
+print(answer)
