727645016e380743760e26028e002f9b856cf1dd
BaeChangho <billyly96@gmail.com>
2022-05-03 14:19:24 +0900

    2주차

--- 2주차/16916.py
+++ 2주차/16916.py
@@ -9,6 +9,7 @@ for i in range(len(S)):
     if len(S)-i< len(P):
         break
     if S[i] == P[0]:
+        answer = 1
         for j in range(len(P)-1):
             if S[i+1+j] == P[j+1]:
                 answer = 1
